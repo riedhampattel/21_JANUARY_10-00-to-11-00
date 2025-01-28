8d6bfab1ffb14dce1865b0ebb64e2484d9a55d9b
riedhampattel <riedhampattel.tops@gmail.com>
2025-01-28 10:10:35 +0530

    practice

---
+++ C Programming/Basic Syntax/1.c
@@ -0,0 +1,17 @@
+//sigle line comment
+/*
+	multiline comments
+	# ---> Pre processor
+	include ---> directive
+	stdio.h ---> standard input output header file
+	int ---> return type
+	main ---> function name
+*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello World");
+	printf("\n");
+	printf("How are you?");
+	return 0;
+}
---
+++ C Programming/Basic Syntax/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,d=30;
+	float f=5.63;
+	char c='A';
+	
+	printf("Value of a = %d",a);
+	printf("\nValue of f = %.4f",f);
+	printf("\nValue of c = %c",c);
+	return 0;
+}
