85c3b9ada0d815e2e07df44fdd2eae5ae5e5df3e
riedhampattel <riedhampattel.tops@gmail.com>
2025-02-11 13:13:44 +0530

    updated

--- C Programming/Loops/4.c
+++ C Programming/Loops/4.c
@@ -4,15 +4,15 @@ int main()
 	int n1,n2,i,j;
 	printf("\nPlease enter the numbers in multiply of 10");
 	printf("\nEnter the starting point = ");
-	scanf("%d",&n1);
+	scanf("%d",&n1);//10
 	printf("\nEnter the ending point = ");
-	scanf("%d",&n2);
+	scanf("%d",&n2);//60
 	int temp = n1+1;
 	int copy = n1+1;
-	for(i=1;i<=10;i++)
+	for(i=1;i(2)<=10;i++)
 	{
 		temp = copy;
-		for(j=1;j<=(n2-n1)/10;j++)
+		for(j=1;j(6)<=(n2-n1)/10;j++)
 		{
 			if(j!=1)
 			{
