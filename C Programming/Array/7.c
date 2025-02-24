a97e2adb692a8fb0bba337dcccc2025dfabc61e9
riedhampattel <riedhampattel.tops@gmail.com>
2025-02-24 10:30:51 +0530

    updated

--- C Programming/Array/7.c
+++ C Programming/Array/7.c
@@ -1,1 +1,20 @@
 //Write a c program to take size of an array and elements from the user and print the sum and average of all the elements
+#include<stdio.h>
+int main()
+{
+	int a[100],i,size,sum=0;
+	printf("\nEnter the size of an array = ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		printf("\nEnter the element in a[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		sum = sum + a[i];
+	}
+	printf("\nThe sum of the elements is = %d",sum);
+	printf("\nThe average of the elements is = %.2f",(float)sum/(float)size);
+	return 0;
+}
--- C Programming/Array/8.c
+++ C Programming/Array/8.c
@@ -1,1 +1,34 @@
 //Write a c program to take size of an array and elements from the user and display only unique elements of an array.
+#include<stdio.h>
+int main()
+{
+	int a[100],size,i,j;
+	printf("\nEnter the size of an array = ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		printf("\nEnter the elements in a[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	printf("\nUnique elements = ");
+	for(i=0;i<size;i++)
+	{
+		int count=0;
+		for(j=0;j<size;j++)
+		{
+			if(i!=j)
+			{
+				if(a[i]==a[j])
+				{
+					count=1;
+					break;	
+				}	
+			}
+		}
+		if(count==0)
+		{
+			printf("%d ",a[i]);
+		}
+	}
+	return 0;
+}
