1f0dd22127974e442b13de96fd97abcb257330cb
riedhampattel <riedhampattel.tops@gmail.com>
2025-03-31 12:40:45 +0530

    updated

--- C++/Concepts/7.cpp
+++ C++/Concepts/7.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 //:: ---> scope resolution operator
+using namespace std;
 int main()
 {
+    string name;
     std::cout<<"Hello Word";
+    cout<<"\nEnter the string = ";
+    cin>>name;
+    cout<<"\nName = "<<name;
     return 0;
 }
