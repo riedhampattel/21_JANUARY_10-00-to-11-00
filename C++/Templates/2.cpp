4fbde5e46d531395f624fc6cb369fd1d874944a9
riedhampattel <riedhampattel.tops@gmail.com>
2025-04-01 12:52:55 +0530

    updated

--- C++/Templates/2.cpp
+++ C++/Templates/2.cpp
@@ -3,12 +3,23 @@ using namespace std;
 template <typename T,int size>
 void sort(T (&arr)[size])
 {
-    
+    T temp;
+    for(i=0;i<size;i++)
+    {
+        for(j=i+1;j<size;j++)
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
 }
 int main()
 {
     int a[5] = {3,2,6,1,4};
-    sort(a);
+    cout<<"\nArray before sorting = ";
     
+    sort(a);
+
     return 0;
 }
